0cc96b568617d248f4c86ba94a6a284455c1bb11
Ivan Porto Wigner <47715589+iivvaannxx@users.noreply.github.com>
2024-09-06 20:30:28 +0200

    refactor: fixed package import

--- src/test/java/com/adobe/bookstore/BookStockResourceTest.java
+++ src/test/java/com/adobe/bookstore/BookStockResourceTest.java
@@ -7,6 +7,7 @@ import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.boot.test.web.server.LocalServerPort;
 import org.springframework.test.context.jdbc.Sql;
 
+import com.adobe.bookstore.bookstock.BookStock;
 import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
